56cc161d1517ada11389f7054b75df627c957267
Wes Smith <wes@w5mith.com>
2012-09-22 14:12:57 -0400

    Added a static library target

---
+++ MUOAuth2ClientTests/MUOAuth2ClientTests.h
@@ -0,0 +1,13 @@
+//
+//  MUOAuth2ClientTests.h
+//  MUOAuth2ClientTests
+//
+//  Created by Wes on 9/20/12.
+//  Copyright (c) 2012 Wesley Smith. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface MUOAuth2ClientTests : SenTestCase
+
+@end
---
+++ MUOAuth2ClientTests/MUOAuth2ClientTests.m
@@ -0,0 +1,32 @@
+//
+//  MUOAuth2ClientTests.m
+//  MUOAuth2ClientTests
+//
+//  Created by Wes on 9/20/12.
+//  Copyright (c) 2012 Wesley Smith. All rights reserved.
+//
+
+#import "MUOAuth2ClientTests.h"
+
+@implementation MUOAuth2ClientTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in MUOAuth2ClientTests");
+}
+
+@end
