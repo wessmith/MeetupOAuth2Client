61947c6455a9f33e7447e898c741087d8db648ca
Wes Smith <wes@w5mith.com>
2012-09-23 17:52:44 -0400

    House keeping

--- MUOAuth2LoginView.m
+++ MUOAuth2LoginView.m
@@ -14,11 +14,16 @@ const CGFloat kPadding = 8.0;
 const NSTimeInterval kShowAnimationDuration = 0.2;
 const NSTimeInterval kBounceAnimationDuration = 0.15;
 
+///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
 #pragma mark - Close Button -
 
 @interface MUCloseButton : UIControl
 @end
 
+///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
 @implementation MUCloseButton
 
 - (id)initWithFrame:(CGRect)frame
@@ -61,11 +66,10 @@ const NSTimeInterval kBounceAnimationDuration = 0.15;
 
 @end
 
-
+///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
 #pragma mark - Login View -
 
-////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////
 @interface MUOAuth2LoginView() <UIWebViewDelegate>
 @property (nonatomic, weak) id <MUOAuth2LoginViewDelegate> delegate;
 @property (nonatomic, strong) UIWebView *webView;
@@ -73,11 +77,12 @@ const NSTimeInterval kBounceAnimationDuration = 0.15;
 @property (nonatomic, strong) MUCloseButton *closeButton;
 @end
 
-////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
 @implementation MUOAuth2LoginView
 
-////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
 - (id)initWithRequest:(NSURLRequest *)request
              delegate:(id <MUOAuth2LoginViewDelegate>)delegate;
 {
@@ -91,12 +96,19 @@ const NSTimeInterval kBounceAnimationDuration = 0.15;
         UIApplication *app = [UIApplication sharedApplication];
         UIWindow *window = app.keyWindow;
         CGFloat statusBarheight = app.statusBarFrame.size.height;
-        UIEdgeInsets contentInsets = UIEdgeInsetsMake(kPadding + statusBarheight, kPadding, kPadding, kPadding);
+        UIEdgeInsets contentInsets = UIEdgeInsetsMake(kPadding + statusBarheight,
+                                                      kPadding,
+                                                      kPadding,
+                                                      kPadding);
+        
         self.frame = UIEdgeInsetsInsetRect(window.bounds, contentInsets);
         [window addSubview:self];
         
         // Add an off-white background view.
-        UIView *backgroundView = [[UIView alloc] initWithFrame:CGRectInset(self.bounds, kPadding, kPadding)];
+        UIView *backgroundView = [[UIView alloc] initWithFrame:CGRectInset(self.bounds,
+                                                                           kPadding,
+                                                                           kPadding)];
+        
         backgroundView.backgroundColor = [UIColor colorWithWhite:0.97 alpha:1.f];
         backgroundView.layer.cornerRadius = 5.f;
         backgroundView.clipsToBounds = YES;
@@ -113,17 +125,18 @@ const NSTimeInterval kBounceAnimationDuration = 0.15;
     return self;
 }
 
-////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
 - (void)dealloc
 {
     // Recommended in the Apple docs.
     self.webView.delegate = nil;
 }
 
+///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
+#pragma mark - Getters
 
-#pragma mark - Getters -
-
-////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
 - (UIWebView *)webView
 {
     if (!_webView) {
@@ -135,11 +148,12 @@ const NSTimeInterval kBounceAnimationDuration = 0.15;
     return _webView;
 }
 
-////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
 - (UIActivityIndicatorView *)activityView
 {
     if (!_activityView) {
-        _activityView = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhiteLarge];
+        _activityView = [[UIActivityIndicatorView alloc]
+                         initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhiteLarge];
         _activityView.color = [UIColor grayColor];
         _activityView.center = self.center;
         _activityView.hidesWhenStopped = YES;
@@ -147,22 +161,26 @@ const NSTimeInterval kBounceAnimationDuration = 0.15;
     return _activityView;
 }
 
-////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
 - (MUCloseButton *)closeButton
 {
     if (!_closeButton) {
-        _closeButton = [[MUCloseButton alloc] initWithFrame:CGRectMake(self.frame.size.width, -14, 44, 44)];
+        _closeButton = [[MUCloseButton alloc] initWithFrame:
+                        CGRectMake(self.frame.size.width, -14, 44, 44)];
         _closeButton.transform = CGAffineTransformMakeScale(0.001, 0.001);
-        [_closeButton addTarget:self action:@selector(close) forControlEvents:UIControlEventTouchUpInside];
+        [_closeButton addTarget:self action:@selector(close)
+               forControlEvents:UIControlEventTouchUpInside];
     }
     return _closeButton;
 }
 
-////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
 - (void)show
 {
     // Animate slightly larger than target scale...
-    [UIView animateWithDuration:kShowAnimationDuration delay:0 options:UIViewAnimationOptionCurveLinear animations:^{
+    [UIView animateWithDuration:kShowAnimationDuration
+                          delay:0
+                        options:UIViewAnimationOptionCurveLinear animations:^{
         
         self.transform = CGAffineTransformMakeScale(1.1, 1.1);
         self.alpha = 1.f;
@@ -173,7 +191,9 @@ const NSTimeInterval kBounceAnimationDuration = 0.15;
         [self.activityView startAnimating];
         
         // Bounce back to slightly smaller than target scale...
-        [UIView animateWithDuration:kBounceAnimationDuration delay:0 options:UIViewAnimationOptionCurveLinear animations:^{
+        [UIView animateWithDuration:kBounceAnimationDuration
+                              delay:0
+                            options:UIViewAnimationOptionCurveLinear animations:^{
             
             self.transform = CGAffineTransformMakeScale(0.9, 0.9);
             
@@ -183,30 +203,34 @@ const NSTimeInterval kBounceAnimationDuration = 0.15;
             [self addSubview:self.closeButton];
             
             // Final bounce to target scale.
-            [UIView animateWithDuration:kBounceAnimationDuration delay:0 options:UIViewAnimationOptionCurveEaseOut animations:^{
+            [UIView animateWithDuration:kBounceAnimationDuration
+                                  delay:0
+                                options:UIViewAnimationOptionCurveEaseOut animations:^{
 
                 self.transform = CGAffineTransformMakeScale(1, 1);
                 self.closeButton.transform = CGAffineTransformMakeScale(1, 1);
                 
-            } completion:^(BOOL finished) {
-                
-            }];
+            } completion:NULL];
         }];
     }];
 }
 
-////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
 - (void)close
 {
     // Bounce slightly larger than target scale...
-    [UIView animateWithDuration:kBounceAnimationDuration delay:0 options:UIViewAnimationOptionCurveEaseOut animations:^{
+    [UIView animateWithDuration:kBounceAnimationDuration
+                          delay:0
+                        options:UIViewAnimationOptionCurveEaseOut animations:^{
        
         self.transform = CGAffineTransformMakeScale(1.1, 1.1);
        
     } completion:^(BOOL finished) {
         
         // Animate down to target scale.
-        [UIView animateWithDuration:kBounceAnimationDuration delay:0 options:UIViewAnimationOptionCurveEaseOut animations:^{
+        [UIView animateWithDuration:kBounceAnimationDuration
+                              delay:0
+                            options:UIViewAnimationOptionCurveEaseOut animations:^{
             
             self.transform = CGAffineTransformMakeScale(0.001, 0.001);
             self.alpha = 0.f;
@@ -218,35 +242,31 @@ const NSTimeInterval kBounceAnimationDuration = 0.15;
     }];
 }
 
+///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
+#pragma mark - Web View Delegate -
 
-#pragma mark - Web View Delegate
-
-////////////////////////////////////////////////////////////////////////////////
-- (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType
-{
-    NSLog(@"Request URI: %@", request.URL);
-    
+///////////////////////////////////////////////////////////////////////////////////////////////////
+- (BOOL)webView:(UIWebView *)webView
+            shouldStartLoadWithRequest:(NSURLRequest *)request
+                        navigationType:(UIWebViewNavigationType)navigationType
+{    
     if ([self.delegate conformsToProtocol:@protocol(MUOAuth2LoginViewDelegate)])
         return [self.delegate loginView:self shouldStartLoadWithRequest:request];
     
     return YES;
 }
 
-////////////////////////////////////////////////////////////////////////////////
-- (void)webViewDidStartLoad:(UIWebView *)webView
-{
-    NSLog(@"Started loading authorization view...");
-}
-
-////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
 - (void)webViewDidFinishLoad:(UIWebView *)webView
-{
-    NSLog(@"Finished loading authorization view.");
-    
+{    
     [self.activityView stopAnimating];
     
     // Adjust the width of the webview content to fit nicely in the slightly smaller frame.
-    NSString *javascript = [NSString stringWithFormat:@"document.body.style.width='%dpx'", (int)self.webView.bounds.size.width];
+    NSString *javascript = [NSString stringWithFormat:
+                                @"document.body.style.width='%dpx'",
+                                (int)self.webView.bounds.size.width];
+    
     [self.webView stringByEvaluatingJavaScriptFromString:javascript];
     
     // Add the webView.
